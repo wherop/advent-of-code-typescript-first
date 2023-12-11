52c43fca5f0af301c97a166b02f5c53bc1831569
Péter T. Winkler <peter.t.winkler@gmail.com>
2023-12-11 07:27:46 +0100

    initialised Day 11

---
+++ 2023/src/day11/index.ts
@@ -0,0 +1,48 @@
+import run from 'aocrunner';
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: 
+`...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`,
+        expected: 374,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: '',
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});
